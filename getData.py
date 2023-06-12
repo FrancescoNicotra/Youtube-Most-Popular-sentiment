670f9cc6121989b2887387f63633d2e35ac9de7e
Francesco Nicotra <84009233+FrancescoNicotra@users.noreply.github.com>
2023-06-12 16:29:43 +0200

    Completed project

--- getData.py
+++ getData.py
@@ -1,104 +1,99 @@
 import argparse
 import os
-import re
 import json
 import requests
 import googleapiclient.discovery
 from dotenv import load_dotenv
-import google.oauth2.credentials
-import google_auth_oauthlib.flow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from google_auth_oauthlib.flow import InstalledAppFlow
-import uuid
 
+# Load environment variables
 load_dotenv('.env')
-CLIENT_SECRETS_FILE = os.getenv('CLIENT')
-
-# This OAuth 2.0 access scope allows for read-only access to the authenticated
-# user's account, but not other types of account access.
-SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
+API_KEY = os.getenv('API_KEY')
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
-DEVELOPER_KEY = os.getenv('API_KEY')
+CSV_FILENAME = 'trending_videos.csv'
+COUNTRIES = ['IT']  # List of countries to analyze
 
-VALID_BROADCAST_STATUSES = ('all', 'active', 'completed', 'upcoming',)
+# authenticate to the API
 
 
-# Authorize the request and store authorization credentials.
 def get_authenticated_service():
-    flow = InstalledAppFlow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, SCOPES)
-    credentials = flow.run_local_server()
-    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials, cache_discovery=False)
+    return build(API_SERVICE_NAME, API_VERSION, developerKey=API_KEY, cache_discovery=False)
+
+# send data to logstash
 
 
 def send_to_logstash(data):
-    logstash_url = 'http://localhost:9090'  # URL di Logstash
+    logstash_url = 'http://localhost:9090'
 
     try:
-        response = requests.post(logstash_url, data=data)
+        response = requests.post(logstash_url, json=data)
         if response.status_code == 200:
             print("Dati inviati a Logstash con successo.")
         else:
-            print("Errore durante l'invio dei dati a Logstash. Codice di risposta:", response.status_code)
+            print("Errore durante l'invio dei dati a Logstash. Codice di risposta:",
+                  response.status_code)
     except requests.exceptions.RequestException as e:
         print("Errore durante l'invio dei dati a Logstash:", str(e))
+
+# list trending videos and retrieve comments
 
 
 def list_trending_videos(youtube):
     videos = []
 
-    youtubeAPI = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, developerKey=DEVELOPER_KEY)
-    region_codes = ['US', 'IT']  # Codici di regione per gli Stati Uniti e l'Italia
+    youtubeAPI = googleapiclient.discovery.build(
+        API_SERVICE_NAME, API_VERSION, developerKey=API_KEY)
 
-    for region_code in region_codes:
+    for country in COUNTRIES:
         search_response = youtube.videos().list(
             part='snippet,statistics',
             chart='mostPopular',
-            regionCode=region_code,
-            maxResults=15
+            maxResults=10,
+            regionCode=country
         ).execute()
 
         for item in search_response.get('items', []):
-            video = item['snippet']
             video_id = item['id']
-            video_statistics = item.get('statistics', {})
+            video_title = item['snippet']['title']
             video_item = {
-                'title': video['title'],
                 'videoId': video_id,
-                'views': int(video_statistics.get('viewCount', 0)),
-                'doc_id': str(uuid.uuid4().int),  # Genera un UUID intero univoco
-                'region': region_code  # Aggiunge il codice di regione al JSON
+                'videoTitle': video_title,
+                'comments': []
             }
 
-            request = youtubeAPI.commentThreads().list(
-                part="snippet",
-                videoId=video_id,
-                maxResults=100
-            )
-            response = request.execute()
+            try:
+                request = youtubeAPI.commentThreads().list(
+                    part="snippet",
+                    videoId=video_id,
+                    maxResults=100
+                )
+                response = request.execute()
 
-            # for comment_thread in response.get('items', []):
-            #   comment = comment_thread['snippet']['topLevelComment']['snippet']
-            #  video_item['comments'].append(comment['textDisplay'])
+                for comment_thread in response.get('items', []):
+                    comment = comment_thread['snippet']['topLevelComment']['snippet']
+                    comment_item = {
+                        'videoId': video_id,
+                        'videoTitle': video_title,
+                        'created_at': comment['publishedAt'],
+                        'comment': comment['textDisplay']
+                    }
+                    videos.append(comment_item)
+            except HttpError as e:
+                error_message = json.loads(e.content)['error']['message']
+                if 'disabled comments' in error_message:
+                    print(
+                        f"I commenti sono disabilitati per il video con ID '{video_id}'.")
+                else:
+                    print(
+                        f"Errore durante il recupero dei commenti per il video con ID '{video_id}':", error_message)
 
-            videos.append(video_item)
-
-    formatted_json = json.dumps(videos, indent=4)
-    print(formatted_json)
-
-    # Invia i dati a Logstash tramite una richiesta POST
-    logstash_url = 'http://localhost:9090'
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(logstash_url, data=formatted_json, headers=headers)
-
-    if response.status_code == 200:
-        print('Dati inviati con successo a Logstash.')
-    else:
-        print('Errore durante l\'invio dei dati a Logstash.')
+    for comment in videos:
+        send_to_logstash(comment)
 
 
+# main
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
@@ -107,4 +102,5 @@ if __name__ == '__main__':
     try:
         list_trending_videos(youtube)
     except HttpError as e:
-        print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
+        print('Si è verificato un errore HTTP %d:\n%s' %
+              (e.resp.status, e.content))
--- spark/spark.py
+++ spark/spark.py
@@ -1,57 +1,101 @@
 from __future__ import print_function
-import sys
-import json
+from pyspark.sql.functions import count, when, col, date_format, from_json
 from elasticsearch import Elasticsearch
 from pyspark import SparkContext
-from pyspark.sql import SparkSession
-from pyspark.streaming import StreamingContext
-from pyspark.sql.dataframe import DataFrame
-import time
+from pyspark.sql.session import SparkSession
+from pyspark.conf import SparkConf
+import json
+from pyspark import SparkContext
+import pyspark.sql.types as tp
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer, StopWordsRemover
+from pyspark.ml.classification import LogisticRegression
 
-
-def elaborate(batch_df: DataFrame, batch_id: int):
-    batch_df.show(truncate=False)
-
-
-# Funzione per inviare i dati a Elasticsearch
-sc = SparkContext(appName="PythonStructuredStreamsKafka")
-spark = SparkSession(sc)
-print(spark.version)
-sc.setLogLevel("WARN")
-
+elastic_host = "http://elasticsearch:9200"
+elastic_index = "videos"
 kafkaServer = "kafkaServer:9092"
 topic = "youtap"
 
-
-# Funzione per inviare i dati a Elasticsearch
-def send_to_elasticsearch(batch_df: DataFrame, batch_id: int):
-    es = Elasticsearch({'host': 'elasticsearch', 'port': 9200, 'use_ssl': False}, timeout=30,
-                       max_retries=10, retry_on_timeout=True)  # Indirizzo IP o hostname di Elasticsearch
-    if not es.ping():
-        raise ValueError("Impossibile connettersi a Elasticsearch")
-
-    # Conversione del DataFrame in una lista di dizionari
-    records = batch_df.toJSON().map(json.loads).collect()
-
-    # Invio dei dati a Elasticsearch
-    for record in records:
-        converted_dict = json.loads(record["value"])
-
-        # Utilizza l'id come id del documento in Elasticsearch
-        doc_id = int(str(time.time()).replace(".", ""))
-
-        update_body = {
-            "doc": converted_dict,
-            "doc_as_upsert": True
+es_mapping = {
+    "mappings": {
+        "properties": {
+            "created_at": {"type": "date", "format": "yyyy-MM-dd'T'HH:mm:ss"},
+            "comment": {"type": "text", "fielddata": True},
+            "positive_percentage": {"type": "float"},
+            "negative_percentage": {"type": "float"}
         }
+    }
+}
 
-        es.update(index="videos", id=doc_id, body=update_body)
-        # es.index(index="movies", id=doc_id, body=converted_dict)
+es = Elasticsearch(hosts=elastic_host, verify_certs=False)
+response = es.indices.create(
+    index=elastic_index,
+    body=es_mapping,
+    ignore=400
+)
 
-# Streaming Query
+if 'acknowledged' in response:
+    if response['acknowledged'] == True:
+        print("INDEX MAPPING SUCCESS FOR INDEX:", response['index'])
+
+# create traingin set
+youtap = tp.StructType([
+    tp.StructField(name='created_at',
+                   dataType=tp.TimestampType(), nullable=True),
+    tp.StructField(name='videoTitle', dataType=tp.StringType(), nullable=True),
+    tp.StructField(name='comment', dataType=tp.StringType(), nullable=True),
+    tp.StructField(name='videoId', dataType=tp.StringType(), nullable=True),
+])
+
+# create schema for training set
+schema = tp.StructType([
+    tp.StructField(name='id', dataType=tp.StringType(), nullable=True),
+    tp.StructField(name='subjective',
+                   dataType=tp.IntegerType(), nullable=True),
+    tp.StructField(name='positive', dataType=tp.IntegerType(), nullable=True),
+    tp.StructField(name='negative', dataType=tp.IntegerType(), nullable=True),
+    tp.StructField(name='ironic', dataType=tp.IntegerType(), nullable=True),
+    tp.StructField(name='lpositive', dataType=tp.IntegerType(), nullable=True),
+    tp.StructField(name='lnegative', dataType=tp.IntegerType(), nullable=True),
+    tp.StructField(name='top', dataType=tp.IntegerType(), nullable=True),
+    tp.StructField(name='comment', dataType=tp.StringType(), nullable=True)
+])
 
 
-# Streaming Query
+# create spark session
+sparkConf = SparkConf()
+sc = SparkContext(appName="TapSentiment", conf=sparkConf)
+spark = SparkSession(sc)
+
+sc.setLogLevel("ERROR")
+
+print("Sto leggendo il traingin set...")
+training_set = spark.read.csv(
+    'training_set_sentipolc16.csv', schema=schema, header=True, sep=','
+)
+print("Finito.")
+
+# create pipeline
+tokenizer = Tokenizer(inputCol="comment", outputCol="words")
+ita = StopWordsRemover.loadDefaultStopWords("italian")
+stopWords = StopWordsRemover(
+    inputCol='words', outputCol='filtered_words', stopWords=ita
+)
+hashtf = HashingTF(numFeatures=2**16,
+                   inputCol="filtered_words", outputCol='tf')
+idf = IDF(inputCol='tf', outputCol="features", minDocFreq=5)
+model = LogisticRegression(featuresCol='features', labelCol='positive')
+pipeline = Pipeline(stages=[tokenizer, stopWords, hashtf, idf, model])
+
+print("Sto addestrando il modello...")
+pipelineFit = pipeline.fit(training_set)
+print("Ho finito di addestrare il modello.")
+
+modelSummary = pipelineFit.stages[-1].summary
+print("Accuratezza del modello:")
+print(modelSummary.accuracy)
+
+print("Sono pronto a leggere streaming da kafka...")
 df = spark \
     .readStream \
     .format("kafka") \
@@ -59,65 +103,59 @@ df = spark \
     .option("subscribe", topic) \
     .load()
 
-df.selectExpr("CAST(timestamp AS STRING)", "CAST(value AS STRING)") \
-  .writeStream \
-  .foreachBatch(send_to_elasticsearch) \
-  .start() \
-  .awaitTermination()
+df = df.selectExpr("CAST(value AS STRING)") \
+    .select(from_json("value", youtap).alias("data")) \
+    .select("data.*")
 
-# df.writeStream \
-#  .format("console") \
-#  .start() \
-#  .awaitTermination()
-'''
-df.selectExpr("CAST(timestamp AS STRING)","CAST(value AS STRING)") \
-  .writeStream \
-  .foreachBatch(send_to_elasticsearch) \
-  .start() \
-  .awaitTermination()
+# Modifica il formato di created_at
+df = df.withColumn("created_at", date_format(
+    "created_at", "yyyy-MM-dd'T'HH:mm:ss"))
 
-Mirko, [15/05/2023 22:01]
-# Funzione per inviare i dati a Elasticsearch
-def send_to_elasticsearch(batch_df: DataFrame, batch_id: int):
-    es = Elasticsearch({'host': 'elasticsearch', 'port': 9200, 'use_ssl': False}, timeout=30, max_retries=10, retry_on_timeout=True)  # Indirizzo IP o hostname di Elasticsearch
-    if not es.ping():
-        raise ValueError("Impossibile connettersi a Elasticsearch")
+df = pipelineFit.transform(df) \
+    .select("created_at", "videoTitle", "videoId", "comment", "prediction")
 
-    # Conversione del DataFrame in una lista di dizionari
-    records = batch_df.toJSON().map(json.loads).collect()
 
-    # Invio dei dati a Elasticsearch
+def calculate_percentages(df):
+    positive_comments = df.groupBy("videoId").agg(
+        count(when(col("prediction") == 1.0, True)).alias("positive_comments")
+    )
+
+    negative_comments = df.groupBy("videoId").agg(
+        count(when(col("prediction") == 0.0, True)).alias("negative_comments")
+    )
+
+    df_with_percentages = df.join(positive_comments, "videoId").join(
+        negative_comments, "videoId")
+
+    df_with_percentages = df_with_percentages.withColumn(
+        "percentuale_positivi", (col(
+            "positive_comments") / (col("positive_comments") + col("negative_comments"))) * 100
+    ).withColumn(
+        "percentuale_negativi", (col(
+            "negative_comments") / (col("positive_comments") + col("negative_comments"))) * 100
+    )
+
+    return df_with_percentages
+
+# Send to Elasticsearch
+
+
+def send_to_elasticsearch(batch_df, batch_id):
+    batch_df = calculate_percentages(batch_df)
+    batch_df = batch_df.withColumn("percentuale_positivi", col(
+        "percentuale_positivi").cast("double"))
+    batch_df = batch_df.withColumn("percentuale_negativi", col(
+        "percentuale_negativi").cast("double"))
+
+    json_records = batch_df.toJSON().collect()
+    records = [json.loads(record) for record in json_records]
+
     for record in records:
-        converted_dict = json.loads(record["value"])
-
-        if "dailyData" not in converted_dict:
-            converted_dict["dailyData"] = []
-
-        converted_dict["dailyData"].append({
-          "popularity": converted_dict["popularity"],
-          "vote_average": converted_dict["vote_average"],
-          "vote_count": converted_dict["vote_count"],
-          "date": datetime.today().strftime('%Y-%m-%d')
-         })
-
-        converted_dict["dailyData"].append({
-          "popularity": converted_dict["popularity"]+50,
-          "vote_average": converted_dict["vote_average"]+100,
-          "vote_count": converted_dict["vote_count"]+20,
-          "date": (datetime.today()+timedelta(days=1)).strftime('%Y-%m-%d')
-         })
-
-        print(converted_dict)
-        # print(type(converted_dict))
+        es.index(index=elastic_index, document=record)
 
 
-        doc_id = converted_dict["id"]  # Utilizza l'id come id del documento in Elasticsearch
-
-        update_body = {
-            "doc": converted_dict,
-            "doc_as_upsert": True
-        }
-
-        es.update(index="movies", id=doc_id, body=update_body)
-        # es.index(index="movies", id=doc_id, body=converted_dict)
-'''
+# Write to Elasticsearch
+df.writeStream \
+    .foreachBatch(send_to_elasticsearch) \
+    .start() \
+    .awaitTermination()
